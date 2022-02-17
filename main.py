dc7638e66a4751b7bbdc96a7273fc1908f777d9f
Besto9945 <84808974+Besto9945@users.noreply.github.com>
2022-02-17 18:47:15 +0700

    first

---
+++ main.py
@@ -0,0 +1,106 @@
+from fastapi import FastAPI, HTTPException
+from pymongo import MongoClient
+from typing import Optional
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+client = MongoClient('mongodb://localhost', 27017)
+db =client["user-data"]
+collection1 = db["user"]
+
+
+class user(BaseModel):
+    username: str
+    password: str
+    email: str
+    SerialNumber: str
+
+
+# register new user
+@app.post("/user-register/")
+def user_register(u: user):
+    query1 = {
+        "username": u.username,
+    }
+    query2 = {
+        "SerialNumber": u.SerialNumber
+    }
+
+
+    check_username = collection1.find_one(query1, {"_id": 0})
+    check_SerialNumber = collection1.find_one(query2, {"_id": 0})
+
+    if check_username is None and check_SerialNumber is None:
+        user = {
+            "username": u.username,
+            "password": u.password,
+            "SerialNumber": u.SerialNumber,
+            "bt1": None,
+            "bt2": None,
+            "bt3": None,
+            "bt4": None,
+            "BreakText": "Break!!!",
+            "LightBrokenText": "ไฟเสีย"
+        }
+        collection1.insert_one(user)
+        return {
+            "result": "register successfully"
+        }
+    else:
+        return {
+            "result": "username or serial number is already use"
+        }
+
+#user information
+@app.get("/get/user-info/")
+def user_login(u: user):
+    query = {
+        "username": u.username,
+        "password": u.password
+    }
+
+    user = collection1.find_one(query, {"_id": 0})
+
+    if user is None:
+        raise HTTPException(404, f"Couldn't find user: {u.username}")
+    
+    return user
+
+# add text to 4 bottoms
+@app.put("/user-update/")
+def add_text_bt(u: user, text1: Optional[str] = None, text2: Optional[str] = None,
+text3: Optional[str] = None, text4: Optional[str] = None):
+    query = {
+        "username": u.username,
+        "password": u.password
+    }
+
+    new_value = {
+        "$set": {
+            "bt1": text1,
+            "bt2": text2,
+            "bt3": text3,
+            "bt4": text4
+        }
+    }
+
+    collection1.update_one(query, new_value)
+    
+    return {
+        "result": "update complete"
+    }
