51555aef4a837e7ef93fc24d81278f1dd4e0cc01
Jakarin Chujan <jojogamer67@gmail.com>
2022-02-17 21:33:29 +0700

    Add add_car method

--- main.py
+++ main.py
@@ -20,27 +20,32 @@ app.add_middleware(
 )
 
 client = MongoClient('mongodb://localhost', 27017)
-db =client["user-data"]
+db = client["user-data"]
 collection1 = db["user"]
+data_car = db['car']
 
 
-class user(BaseModel):
+class User(BaseModel):
     username: str
     password: str
     email: str
     SerialNumber: str
 
 
+class Car(BaseModel):
+    email: str
+    ID: str
+
+
 # register new user
 @app.post("/user-register/")
-def user_register(u: user):
+def user_register(u: User):
     query1 = {
         "username": u.username,
     }
     query2 = {
         "SerialNumber": u.SerialNumber
     }
-
 
     check_username = collection1.find_one(query1, {"_id": 0})
     check_SerialNumber = collection1.find_one(query2, {"_id": 0})
@@ -66,9 +71,10 @@ def user_register(u: user):
             "result": "username or serial number is already use"
         }
 
-#user information
+
+# user information
 @app.get("/get/user-info/")
-def user_login(u: user):
+def user_login(u: User):
     query = {
         "username": u.username,
         "password": u.password
@@ -78,13 +84,14 @@ def user_login(u: user):
 
     if user is None:
         raise HTTPException(404, f"Couldn't find user: {u.username}")
-    
+
     return user
+
 
 # add text to 4 bottoms
 @app.put("/user-update/")
-def add_text_bt(u: user, text1: Optional[str] = None, text2: Optional[str] = None,
-text3: Optional[str] = None, text4: Optional[str] = None):
+def add_text_bt(u: User, text1: Optional[str] = None, text2: Optional[str] = None,
+                text3: Optional[str] = None, text4: Optional[str] = None):
     query = {
         "username": u.username,
         "password": u.password
@@ -100,7 +107,31 @@ text3: Optional[str] = None, text4: Optional[str] = None):
     }
 
     collection1.update_one(query, new_value)
-    
+
     return {
         "result": "update complete"
     }
+
+
+@app.post('/add-car/')
+def add_car(car: Car):
+    query = {"ID": car.ID}
+    check_id_car = data_car.find_one(query, {"_id": 0})
+    if check_id_car is None:
+        car = {"email": car.email,
+               "ID": car.ID,
+               "bt1": None,
+               "bt2": None,
+               "bt3": None,
+               "bt4": None,
+               "break_light": "Break !!",
+               "broken": "ขอโทษครับ ไฟเสียครับ"}
+        c = jsonable_encoder(car)
+        data_car.insert_one(c)
+        return {
+            "result": "The car was added successfully."
+        }
+    else:
+        return {
+            "result": "This car ID already has."
+        }
